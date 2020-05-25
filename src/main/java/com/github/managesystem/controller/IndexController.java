e4d0d38d4e77fb36730ecf4833f8790c9cc7c682
zhangb14 <Zb405133134>
2020-05-25 17:20:49 +0800

    init

---
+++ src/main/java/com/github/managesystem/controller/IndexController.java
@@ -0,0 +1,28 @@
+package com.github.managesystem.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 描述:
+ * ${DESCRIPTION}
+ *
+ * @author Yangjy
+ * @date 2018-04-12
+ */
+@Controller
+public class IndexController {
+
+
+
+    @RequestMapping("/")
+    public String goToindex(HttpServletRequest request, HttpServletResponse response) {
+        System.out.println("aaaa");
+        return "index";
+    }
+
+
+}
